91c70148947c99a27377c75610febe51ea0bb614
Rajendra Jat <jatrj18@gmail.com>
2025-02-05 13:27:50 +0530

    Day-011 : Quote Generator

---
+++ Day-011/src/App.jsx
@@ -0,0 +1,14 @@
+import QuoteBox from "./components/QuoteBox";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Random Quote Generator</h1>
+      <QuoteBox />
+    </>
+  )
+}
+
+export default App
